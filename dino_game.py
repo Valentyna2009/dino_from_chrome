ed1bb82129d01708227259dad4824ee44c862b0a
Valentyna2009 <valiaganzha@gmail.com>
2024-05-28 23:09:26 +0200

    Adding the dino and his animation

--- dino_game.py
+++ dino_game.py
@@ -26,6 +26,41 @@ scroll_speed = 4
 bg = pygame.image.load('dino_from_chrome/bg3.jpg')
 ground = pygame.image.load('dino_from_chrome/dino-ground.png')
 
+i = ground.get_height()
+eto_samoe = SCREEN_HEIGHT - i
+toxyj_nj = (SCREEN_HEIGHT - eto_samoe) + 38
+
+
+class Dino(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 3):
+            images = pygame.image.load(f'dino_from_chrome/DinoRun{num}.png')
+            self.images.append(images)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+
+        # handle the animation
+        self.counter += 0.5
+        dino_cooldown = 5
+
+        if self.counter > dino_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
+
+dino_group = pygame.sprite.Group()
+dinny = Dino(SCREEN_WIDTH / 4, toxyj_nj)
+dino_group.add(dinny)
+
 running = True
 
 while running:
@@ -34,6 +69,10 @@ while running:
     
     # draw background
     screen.blit(bg, (0, 0))
+
+    dino_group.draw(screen)
+    dino_group.update()
+
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 35:
         ground_scroll = 0
@@ -46,7 +85,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # show all the images in the ground
+    # show all the images on the screen
     pygame.display.update()
 
 pygame.quit()
